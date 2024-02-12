952015dbea48a47679f7892d341d2266d675cac7
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-02-12 01:10:41 -0800

    Implement simple game reset when winning

--- src/dom.js
+++ src/dom.js
@@ -1,15 +1,17 @@
 export function displayBoard1(board) {
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board.length; j++) {
-      if (!board[i][j].ship) continue;
       let el = document.querySelector(`#a${i}${j}`);
-      if (!board[i][j].hit) {
+      if (!board[i][j].ship && board[i][j].hit) {
+        el.style.backgroundColor = 'lightgray';
+      }
+      if (board[i][j].ship && !board[i][j].hit) {
         el.style.backgroundColor = 'gray';
       }
-      if (board[i][j].hit) {
+      if (board[i][j].ship && board[i][j].hit) {
         el.style.backgroundColor = 'darkred';
       }
-      if (board[i][j].ship.isSunk()) {
+      if (board[i][j].ship && board[i][j].ship.isSunk()) {
         el.style.backgroundColor = 'red';
       }
     }
@@ -31,4 +33,16 @@ export function displayBoard2(board) {
       }
     }
   }
+}
+
+export function clearBoards() {
+  let cells = document.querySelectorAll('.col1');
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = 'white';
+  });
+
+  cells = document.querySelectorAll('.col2');
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = 'white';
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Gameboard from './gameboard';
-import { displayBoard1, displayBoard2 } from './dom';
+import { displayBoard1, displayBoard2, clearBoards } from './dom';
 import Player from './player';
 
 let board1, board2;
+let bot = new Player();
 
 function getBotMoves(board) {
   let bot = new Player();
@@ -14,21 +15,21 @@ function getBotMoves(board) {
   while (!board.placeShip(bot.getMove(), 5)) {}
 }
 
-function getPlayerMoves(board) {
+function getPlayerMoves() {
   let lens = [2, 3, 3, 4, 5];
   let cells = document.querySelectorAll('.col1');
   cells.forEach((cell) => {
-    cell.addEventListener('click', (e) => addShip(e, board, lens));
+    cell.addEventListener('click', (e) => addShip(e, lens));
   });
 }
-function addShip(e, board, lens) {
+function addShip(e, lens) {
   let id = e.target.getAttribute('id').split('');
   id.shift(); // removes the first char
   let coords = id.map((i) => parseInt(i)); // converts to ints
 
-  if (board.placeShip(coords, lens[0])) {
+  if (board1.placeShip(coords, lens[0])) {
     if (lens.length == 1) allowPlayerAttack();
-    displayBoard1(board.board);
+    displayBoard1(board1.board);
     lens.shift();
   }
 }
@@ -45,9 +46,36 @@ function checkAttackSuccess(e) {
   let id = e.target.getAttribute('id').split('');
   id.shift(); // removes the first char
   let coords = id.map((i) => parseInt(i)); // converts to ints
-  console.log(coords);
-  board2.receiveAttack(coords);
-  displayBoard2(board2.board);
+
+  //  check if tile clicked has already been clicked.
+  if (board2.board[coords[0]][coords[1]].hit) return;
+
+  board2.receiveAttack(coords); // attack
+  displayBoard2(board2.board); // show updated board
+
+  if (board2.allShipsSunk()) {
+    displayWinner('human');
+  }
+
+  allowBotAttack();
+}
+
+function allowBotAttack() {
+  let coords = bot.getMove();
+  while (board1.board[coords[0]][coords[1]].hit) {
+    coords = bot.getMove();
+  }
+  board1.receiveAttack(coords);
+  displayBoard1(board1.board);
+
+  if (board1.allShipsSunk()) {
+    displayWinner('bot');
+  }
+}
+
+function displayWinner(winner) {
+  clearBoards();
+  setup();
 }
 
 function setup() {
@@ -55,7 +83,7 @@ function setup() {
   board2 = new Gameboard();
 
   getBotMoves(board2);
-  getPlayerMoves(board1);
+  getPlayerMoves();
 }
 
 setup();

86c73bad50a3a8bc058b4d9ebd3ea7ca30631e93
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-02-11 17:22:13 -0800

    Implement opposing board visuals for missing hitting and sinking

--- src/dom.js
+++ src/dom.js
@@ -15,3 +15,20 @@ export function displayBoard1(board) {
     }
   }
 }
+
+export function displayBoard2(board) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      let el = document.querySelector(`#b${i}${j}`);
+      if (!board[i][j].ship && board[i][j].hit) {
+        el.style.backgroundColor = 'lightgray';
+      }
+      if (board[i][j].ship && board[i][j].hit) {
+        el.style.backgroundColor = 'darkred';
+      }
+      if (board[i][j].ship && board[i][j].ship.isSunk()) {
+        el.style.backgroundColor = 'red';
+      }
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Gameboard from './gameboard';
-import { displayBoard1 } from './dom';
+import { displayBoard1, displayBoard2 } from './dom';
 import Player from './player';
+
+let board1, board2;
 
 function getBotMoves(board) {
   let bot = new Player();
@@ -19,29 +21,41 @@ function getPlayerMoves(board) {
     cell.addEventListener('click', (e) => addShip(e, board, lens));
   });
 }
-
 function addShip(e, board, lens) {
   let id = e.target.getAttribute('id').split('');
   id.shift(); // removes the first char
   let coords = id.map((i) => parseInt(i)); // converts to ints
 
   if (board.placeShip(coords, lens[0])) {
-    if (lens.length == 1) play();
+    if (lens.length == 1) allowPlayerAttack();
     displayBoard1(board.board);
     lens.shift();
   }
 }
 
+function allowPlayerAttack() {
+  let cells = document.querySelectorAll('.col2');
+  cells.forEach((cell) => {
+    cell.addEventListener('click', (e) => {
+      checkAttackSuccess(e);
+    });
+  });
+}
+function checkAttackSuccess(e) {
+  let id = e.target.getAttribute('id').split('');
+  id.shift(); // removes the first char
+  let coords = id.map((i) => parseInt(i)); // converts to ints
+  console.log(coords);
+  board2.receiveAttack(coords);
+  displayBoard2(board2.board);
+}
+
 function setup() {
-  let board1 = new Gameboard();
-  let board2 = new Gameboard();
+  board1 = new Gameboard();
+  board2 = new Gameboard();
 
   getBotMoves(board2);
   getPlayerMoves(board1);
-}
-
-function play() {
-  console.log('play');
 }
 
 setup();
